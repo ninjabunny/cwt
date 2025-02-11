b99ec4bbc08e5e87d21875b0543663db3152ef05
Kelly Guo <kellyguo@kellys-air.lan>
2025-02-10 21:24:41 -0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR testestsetse
         </p>
       </div>
       <p className="read-the-docs">
