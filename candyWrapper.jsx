e5898d1ea46a642c2cc823ca7e7f124fa394f482
Andee Tao <ninjabunny@gmail.com>
2025-02-14 08:10:26 -0600

    removed some css

--- candyWrapper.jsx
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line react/display-name
-export const candyWrapper = (props) => (WrappedComponent) => {
-  return <WrappedComponent candy={true} {...props} />;
-};
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 /* eslint-disable react/prop-types */
-//TODO: 1. add widget + maybe react testing library
-//TODO: 2. add async await code
-//TODO: 3. What is advantage of reactDOM and react over other libraries
-//TODO: 4. how to position a modal?
 
 import { useState, useEffect, useRef } from "react";
 import GridExample from "./Grid";
@@ -14,17 +10,30 @@ function App() {
   const inputRefs = useRef(null);
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
     if (inputRefs.current) {
       inputRefs.current.getRefs().current[1].focus();
       inputRefs.current.getRefs().current[0].value =
         inputRefs.current.getSomePrivateData();
-      console.log("ref", inputRefs);
     }
   }, []);
 
   return (
     <>
-      <h1>Cross Screen Media POC</h1>
+      <h1 style={{ textAlign: "center" }}>Cross Screen Media POC</h1>
       <div>
         <h2>Complex Tables Demo</h2>
         <GridExample />
@@ -55,9 +64,7 @@ const FancyWidget = ({ input1 = "", input2 = "" }) => {
   return (
     <>
       <h2>Fancy Widget:</h2>
-      <h3
-        style={{ margin: "40px", fontSize: "20px" }}
-      >{`${input1}: ${input2}`}</h3>
+      <h3>{`${input1}: ${input2}`}</h3>
     </>
   );
 };
